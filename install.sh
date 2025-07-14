8bfcf1c520f5de079d679f2443529eb5d93f3ecb
dodistyo <dodipras27@gmail.com>
2025-07-14 22:19:41 +0700

    [util] update base image cde 2

--- install.sh
+++ install.sh
@@ -3,31 +3,18 @@ set -e
 
 echo "[dotfiles] Installing extensions..."
 
-CODE_BIN="/tmp/code-server/bin/code-server"
-
-if [ ! -x "$CODE_BIN" ]; then
-  echo "[dotfiles] ERROR: code-server binary not found at $CODE_BIN"
-  exit 1
-fi
-
 if [ -f "$HOME/dotfiles/extensions.txt" ]; then
   while read -r extension; do
     if [ -n "$extension" ]; then
-      echo "Installing extension: $extension"
-      "$CODE_BIN" --install-extension "$extension" || true
+      echo "[dotfiles] Installing extension: $extension"
+      code-server --install-extension "$extension" || true
     fi
   done < "$HOME/dotfiles/extensions.txt"
 fi
 
-echo "[dotfiles] Copying settings and extention..."
+echo "[dotfiles] Copying settings..."
 
 mkdir -p "$HOME/.local/share/code-server/User"
 cp "$HOME/dotfiles/.vscode/settings.json" "$HOME/.local/share/code-server/User/settings.json"
-
-if [ -d "$HOME/dotfiles/.vscode/extensions" ]; then
-  echo "[dotfiles] Copying extensions folder..."
-  mkdir -p "$HOME/.local/share/code-server/extensions"
-  cp -r "$HOME/dotfiles/.vscode/extensions/"* "$HOME/.local/share/code-server/extensions/"
-fi
 
 echo "[dotfiles] Setup complete."
